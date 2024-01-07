6c00e1e4325452d50e298ddc880bc7cd65eef77f
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-01-07 19:14:39 +0900

    メモ帳アプリのディレクトリ作成

---
+++ src/app/memo/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next'
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Webメモ帳</h1>
+      <div>ここにフォームを作成する</div>
+    </main>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'Webメモ帳',
+}
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const ROUTES = {
+  memo: '/memo',
+} as const
