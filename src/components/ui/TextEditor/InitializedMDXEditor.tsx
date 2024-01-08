f9ab42ad5428488453df3fcc3d39c234c03c8a7e
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-01-08 16:20:58 +0900

    pluginsの抽出

--- src/components/ui/TextEditor/InitializedMDXEditor.tsx
+++ src/components/ui/TextEditor/InitializedMDXEditor.tsx
@@ -1,13 +1,26 @@
+/**
+ * TODO: 次はここから
+ *
+ * - Toolbar Plugin
+ * - Code Blocks Plugin
+ * - Tables Plugin
+ * - Images Plugin
+ * - Error Handling
+ */
+
 'use client'
 
 import {
   BoldItalicUnderlineToggles,
   MDXEditor,
+  RealmPlugin,
   UndoRedo,
   headingsPlugin,
+  linkPlugin,
   listsPlugin,
   markdownShortcutPlugin,
   quotePlugin,
+  tablePlugin,
   thematicBreakPlugin,
   toolbarPlugin,
   type MDXEditorMethods,
@@ -16,29 +29,27 @@ import {
 import '@mdxeditor/editor/style.css'
 import type { ForwardedRef } from 'react'
 
+const plugins = [
+  headingsPlugin(),
+  linkPlugin(),
+  listsPlugin(),
+  markdownShortcutPlugin(),
+  quotePlugin(),
+  thematicBreakPlugin(),
+  toolbarPlugin({
+    toolbarContents: () => (
+      <>
+        <UndoRedo />
+        <BoldItalicUnderlineToggles />
+      </>
+    ),
+  }),
+  tablePlugin(),
+] as const satisfies RealmPlugin[]
+
 export default function InitializedMDXEditor({
   editorRef,
   ...props
 }: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
-  return (
-    <MDXEditor
-      plugins={[
-        headingsPlugin(),
-        listsPlugin(),
-        quotePlugin(),
-        thematicBreakPlugin(),
-        markdownShortcutPlugin(),
-        toolbarPlugin({
-          toolbarContents: () => (
-            <>
-              <UndoRedo />
-              <BoldItalicUnderlineToggles />
-            </>
-          ),
-        }),
-      ]}
-      {...props}
-      ref={editorRef}
-    />
-  )
+  return <MDXEditor plugins={plugins} {...props} ref={editorRef} />
 }
