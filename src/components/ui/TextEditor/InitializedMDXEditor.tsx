fba344c9b63a6f593d09cf7fe75e1b5cb4f8ca95
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-01-08 15:25:24 +0900

    リッチテキストエディターの導入

---
+++ src/components/ui/TextEditor/InitializedMDXEditor.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import {
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  markdownShortcutPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  type MDXEditorMethods,
+  type MDXEditorProps,
+} from '@mdxeditor/editor'
+import type { ForwardedRef } from 'react'
+
+export default function InitializedMDXEditor({
+  editorRef,
+  ...props
+}: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
+  return (
+    <MDXEditor
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+      ]}
+      {...props}
+      ref={editorRef}
+    />
+  )
+}
