66db490997486ede14a5e02993e3f9fb5e3268fd
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-01-09 12:10:25 +0900

    Title.stories作成

--- src/components/ui/Title/Title.stories.tsx
+++ src/components/ui/Title/Title.stories.tsx
@@ -2,12 +2,29 @@ import { Meta, StoryObj } from '@storybook/react'
 import { Title } from './Title'
 
 const meta: Meta<typeof Title> = {
-  component: undefined,
+  argTypes: {
+    order: {
+      control: {
+        type: 'select',
+      },
+      options: [1, 2, 3, 4, 5, 6],
+    },
+  },
+  component: Title,
 }
 export default meta
 
 type Story = StoryObj<typeof Title>
 
-export const Main: Story = {
-  render: () => <Title>My Title</Title>,
+export const Default: Story = {
+  args: {
+    children: 'Title',
+  },
+  render: (args) => (
+    <>
+      <p>Top</p>
+      <Title {...args} />
+      <p>Bottom</p>
+    </>
+  ),
 }
