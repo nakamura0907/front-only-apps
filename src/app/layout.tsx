009bbc6f49867e79975d82d97c6ed1b901f20c68
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-01-10 17:09:57 +0900

    メタ情報を定数に抽出

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
+import '@mantine/core/styles.css'
+import './globals.css'
+
 import { Layout } from '@/components/template/layout'
+import { meta } from '@/config'
 import { AppProvider } from '@/providers'
 import { ColorSchemeScript } from '@mantine/core'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import '@mantine/core/styles.css'
-import './globals.css'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  description: 'フロントエンドだけで動作するNext.jsアプリケーション集',
+  description: meta.description,
   title: {
-    default: 'Front Only Apps',
-    template: '%s | Front Only Apps',
+    default: meta.title.default,
+    template: `%s ${meta.title.separator} ${meta.title.default}`,
   },
 }
 
