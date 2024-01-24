40e49bcaee76eab5f7d58885831bf88c06ea88d6
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-01-24 21:55:59 +0900

    メディア変換：ヘルプテキストの追加

--- src/app/media-converter/page.tsx
+++ src/app/media-converter/page.tsx
@@ -1,6 +1,6 @@
 import { Converter } from '@/app/media-converter/_components'
 import { Heading } from '@/components/modules'
-import { Container } from '@/components/ui'
+import { Container, Text } from '@/components/ui'
 import { routes } from '@/config'
 import { Metadata } from 'next'
 
@@ -8,6 +8,10 @@ export default function Page() {
   return (
     <Container>
       <Heading order={2}>{routes.mediaConverter.label}</Heading>
+      <Text c="gray" mb="sm" size="sm">
+        ご注意&#58;
+        iPhoneの特定バージョンなど一部の端末では、このアプリケーションが正常に動作しない可能性があります。
+      </Text>
       <Converter />
     </Container>
   )
