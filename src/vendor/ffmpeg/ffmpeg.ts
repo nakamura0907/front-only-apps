a452e6355d8b72826d891e95663567c457ee2a3c
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-01-24 22:50:41 +0900

    テスト実装

--- src/vendor/ffmpeg/ffmpeg.ts
+++ src/vendor/ffmpeg/ffmpeg.ts
@@ -1,6 +1,9 @@
 // See: https://ffmpegwasm.netlify.app/docs/getting-started/usage
 import { FFmpeg } from '@ffmpeg/ffmpeg'
 import { fetchFile, toBlobURL } from '@ffmpeg/util'
+import * as E from 'fp-ts/Either'
+import * as T from 'fp-ts/Task'
+import * as TE from 'fp-ts/TaskEither'
 
 const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd'
 
@@ -31,4 +34,19 @@ export const transcodeFile = async (
 
   const data = await ffmpeg.readFile(outputFileName)
   return data
+}
+
+const task =
+  (ffmpeg: FFmpeg): T.Task<void> =>
+  () =>
+    loadFFmpeg(ffmpeg)
+
+const taskEither = (ffmpeg: FFmpeg): TE.TaskEither<Error, void> =>
+  TE.tryCatch(task(ffmpeg), E.toError)
+
+const test = async (ffmpeg: FFmpeg) => {
+  await loadFFmpeg(ffmpeg)
+
+  return (file: Blob | File, inputFileName: string, outputFileName: string) =>
+    transcodeFile(ffmpeg, file, inputFileName, outputFileName)
 }
