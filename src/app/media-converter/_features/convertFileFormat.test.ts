ffdd04648eb17674de01390d06bc70db93bee4fc
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-02-05 10:27:25 +0900

    convertFileFormatテスト

--- src/app/media-converter/_features/convertFileFormat.test.ts
+++ src/app/media-converter/_features/convertFileFormat.test.ts
@@ -1,10 +1,22 @@
+import { FFmpeg, loadFFmpeg, transcodeFile } from '@/vendor'
 import { convertFileFormat } from './convertFileFormat'
 
-describe('Test', () => {
-  it('should pass', () => {
-    console.log(convertFileFormat)
-    // TODO: rewire
+jest.mock('@/vendor')
 
-    expect(1).toBe(1)
+describe('convertFileFormat', () => {
+  it('should convert file format', async () => {
+    const mockFile = new File(['content'], 'sample.mp3')
+    const mockExtension = 'wav'
+    const mockOutputFileName = 'sample.wav'
+    const mockData = new Blob(['data'])
+
+    ;(FFmpeg as jest.Mock).mockImplementation(() => {})
+    ;(loadFFmpeg as jest.Mock).mockResolvedValue(undefined)
+    ;(transcodeFile as jest.Mock).mockResolvedValue(mockData)
+
+    const result = await convertFileFormat(mockFile, mockExtension)
+
+    expect(result).toBeInstanceOf(File)
+    expect(result.name).toBe(mockOutputFileName)
   })
 })
